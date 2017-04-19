ee6df1a0440dcb5479a63402c2778c9925b5bba8
Evan Klitzke <evan@eklitzke.org>
2017-04-19 16:41:02 -0700

    fix some things...

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Demonstration of how to generate mini-batches from a constant loaded into GPU
-memory in Tensorflow. See https://eklitzke.org/pinning-gpu-memory-in-tensorflow
-for the full blog post.
+memory in Tensorflow. This code is written for Python 3. See
+https://eklitzke.org/pinning-gpu-memory-in-tensorflow for the full blog post.
 
 This code is released unencumbered to the public domain; see the LICENSE file
 for details.
--- slice.py
+++ slice.py
@@ -1,7 +1,7 @@
 import random
 
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
 # Height of our input data.
 HEIGHT = 10
@@ -9,40 +9,40 @@ HEIGHT = 10
 # The size of each mini-batch.
 BATCH_SIZE = 2
 
-# Create 10x3 random array in numpy; this lives in main memory (*not* the GPU).
-rand_data = np.random.randn(HEIGHT, 3)
+# Create a 10x3 matrix in numpy; this lives in main memory (*not* the GPU).
+np_data = np.array(range(30)).reshape(10, 3)
 
 print('numpy data (in main memory)')
 print('---------------------------')
-print(rand_data)
+print(np_data)
 print()
 
 # Number of epochs to train for.
 EPOCHS = 100
 
-# Shuffle the indexes of mini-batches, so that this epoch we process the
-# mini-batches in a random order. This helps break locality in the structure
-# of the training dataset, which can help with overfitting.
+# Shuffle the indexes of mini-batches, so that the mini-batches are generated
+# in a random order. This helps break locality in the structure of the training
+# dataset, which can help with overfitting.
 INDEXES = list(range(HEIGHT // BATCH_SIZE))
 random.shuffle(INDEXES)
 
 # Copy the numpy data into TF memory as a constant var; this will be copied
 # exactly one time into the GPU (if one is available).
-c_rand = tf.constant(rand_data, dtype=tf.float32)
+gpu_data = tf.constant(np_data, dtype=tf.float32)
 
 # The index to use when generating our mini-batch.
 ix = tf.placeholder(shape=(), dtype=tf.int32)
 
 # The mini-batch of data we'll work on.
-batch = tf.slice(c_rand, [ix, 0], [BATCH_SIZE, -1])
+batch = tf.slice(gpu_data, [BATCH_SIZE * ix, 0], [BATCH_SIZE, -1])
 
 # The output of the calculation.
 outp = tf.reduce_sum(tf.square(batch))
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for epoch in range(epochs):
-        for i in indexes:
+    for epoch in range(EPOCHS):
+        for i in INDEXES:
             # Run the computation. The only data in the feed_dict is a single
             # 32-bit integer we supply here. All of the data needed for the
             # mini-batch already lives in GPU memory, and doesn't need to be
